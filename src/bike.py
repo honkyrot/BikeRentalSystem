80b6fc07b45b2ec05a83856de34222d9171d9f88
Darian Porch <dporch@ivytech.edu>
2025-12-21 17:22:09 -0500

    Resolve merge conflict in Bike __repr__ and methods

--- src/bike.py
+++ src/bike.py
@@ -1,10 +1,6 @@
 class Bike:
-    def __init__(self, id, make, model, status='available', hourly_rate=5.0):
-        self.id = id
-        self.make = make
-        self.model = model
-        self.status = status  # 'available' or 'rented'
+    def __init__(self, bike_id, make_model, status="available", hourly_rate=10):
+        self.bike_id = bike_id
+        self.make_model = make_model
+        self.status = status
         self.hourly_rate = hourly_rate
-
-    def __repr__(self):
-        return f"Bike({self.id}, {self.make}, {self.model}, {self.status}, ${self.hourly_rate}/hr)"
