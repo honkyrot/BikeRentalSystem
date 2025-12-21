c22c560944f674deb5514598f60ed17699c3ab6d
honkyrot <hrot3756@gmail.com>
2025-12-21 05:09:53 -0500

    mhm
    github broke my fork repository

--- src/customer.py
+++
@@ -1,8 +0,0 @@
-class Customer:
-    def __init__(self, id, name, phone):
-        self.id = id
-        self.name = name
-        self.phone = phone
-
-    def __repr__(self):
-        return f"Customer({self.id}, {self.name}, {self.phone})"
